70c649fda77ce00a02169547c727031a67a8389a
anmolbabu <anmolbudugutta@gmail.com>
2018-02-08 09:56:58 +0530

    Renaming data-api-image to fabric8-hypothesis-data-api-image
    fabric8-hypothesis-bug-id: fabric8-hdd/fabric8-hypothesis-planning#4
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- app_constants.sh
+++ app_constants.sh
@@ -1,3 +1,3 @@
-env_script_location=https://raw.githubusercontent.com/fabric8-hdd/hypothesis-core-image/master/cico_utils/setup_env.sh
-APP_NAME=data-api
+env_script_location=https://raw.githubusercontent.com/fabric8-hdd/fabric8-hypothesis-core-image/master/cico_utils/setup_env.sh
+APP_NAME=fabric8-hypothesis-data-api
 APP_PORT=$(grep "HDD_DATA_API_PORT" openshift/data-api.env | cut -d'=' -f 2)
